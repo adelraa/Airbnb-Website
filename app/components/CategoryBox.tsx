c67953a7fbe5e49844ad6a6347c81d4a35155f19
Adel Ragab <ar1746@fayoum.edu.eg>
2023-12-18 13:04:33 +0200

    change

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,72 +1,72 @@
-// 'use client';
+'use client';
 
-// import qs from 'query-string';
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { useCallback } from "react";
-// import { IconType } from "react-icons";
+import qs from 'query-string';
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { IconType } from "react-icons";
 
-// interface CategoryBoxProps {
-//   icon: IconType,
-//   label: string;
-//   selected?: boolean;
-// }
+interface CategoryBoxProps {
+  icon: IconType,
+  label: string;
+  selected?: boolean;
+}
 
-// const CategoryBox: React.FC<CategoryBoxProps> = ({
-//   icon: Icon,
-//   label,
-//   selected,
-// }) => {
-//   const router = useRouter();
-//   const params = useSearchParams();
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
+  const router = useRouter();
+  const params = useSearchParams();
 
-//   const handleClick = useCallback(() => {
-//     let currentQuery = {};
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
     
-//     if (params) {
-//       currentQuery = qs.parse(params.toString())
-//     }
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
 
-//     const updatedQuery: any = {
-//       ...currentQuery,
-//       category: label
-//     }
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    }
 
-//     if (params?.get('category') === label) {
-//       delete updatedQuery.category;
-//     }
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
 
-//     const url = qs.stringifyUrl({
-//       url: '/',
-//       query: updatedQuery
-//     }, { skipNull: true });
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, { skipNull: true });
 
-//     router.push(url);
-//   }, [label, router, params]);
+    router.push(url);
+  }, [label, router, params]);
 
-//   return ( 
-//     <div
-//       onClick={handleClick}
-//       className={`
-//         flex 
-//         flex-col 
-//         items-center 
-//         justify-center 
-//         gap-2
-//         p-3
-//         border-b-2
-//         hover:text-neutral-800
-//         transition
-//         cursor-pointer
-//         ${selected ? 'border-b-neutral-800' : 'border-transparent'}
-//         ${selected ? 'text-neutral-800' : 'text-neutral-500'}
-//       `}
-//     >
-//       <Icon size={26} />
-//       <div className="font-medium text-sm">
-//         {label}
-//       </div>
-//     </div>
-//    );
-// }
+  return ( 
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">
+        {label}
+      </div>
+    </div>
+   );
+}
  
-// export default CategoryBox;
+export default CategoryBox;
